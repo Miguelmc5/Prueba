d3d9e40072834d6c4202385ee3aec638d1941dae
Miguel Martinez Costa <mmartinezcosta@danielcastelao.org>
2019-10-04 18:05:13 +0200

    Juego adivinar número

---
+++ juego/Juego.java
@@ -0,0 +1,56 @@
+package juego;
+import java.util.*;
+
+public class Juego {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int x=(int)(Math.random()*10);
+	
+	Scanner lectura=new Scanner(System.in);
+	int numeros;
+	int intentos=0;
+	while(intentos<5)
+	{
+	System.out.println("Dime un número: ");
+	numeros=lectura.nextInt();	
+		if(numeros==x)
+		{
+			System.out.println("Eres un tonto con suerte");
+			break;
+		}
+		if(numeros<x)
+			{
+				System.out.println("Te quedaste corto loco");	
+			}
+		
+		if(numeros>x)
+		{
+			System.out.println("Te pasaste animal");	
+		}
+		intentos++;
+	}
+	if(intentos==5)
+	{
+		System.out.println("P14:21 13/03/2019erdiste tonto");
+	}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
